f26b81c34413a68c9f051238384ef8ce7e2b8069
ianpb <ianpaul.budiman@gmail.com>
2022-10-05 10:42:50 +0800

    Create ImportDGtoExcOnline.ps1

---
+++ ImportDGtoExcOnline.ps1
@@ -0,0 +1,1 @@
+import-csv "c:\temp\E5Members.csv" | foreach {New-DistributionGroup -name $_.name -DisplayName $_.name -ManagedBy $_.managedby.Split(",") -PrimarySmtpAddress $_.mail -members $_.members.Split(",") -MemberJoinRestriction 'ApprovalRequired' -MemberDepartRestriction "Closed" -RequireSenderAuthenticationEnabled $false}
