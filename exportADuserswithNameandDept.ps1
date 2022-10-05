b9cba1ce4e903b0583d4d4c73cd829439530f73e
ianpb <ianpaul.budiman@gmail.com>
2022-10-05 10:55:21 +0800

    Update exportADuserswithNameandDept.ps1

--- exportADuserswithNameandDept.ps1
+++ exportADuserswithNameandDept.ps1
@@ -1,2 +1,2 @@
 #Export AD Users with Name and Dept
-Get-ADUser -Filter * -SearchBase "OU=Users,OU=CCI,DC=office,DC=login" -Properties * | Select-Object name,department | export-csv -path c:\temp\userexportAll.csv
+Get-ADUser -Filter * -SearchBase "OU=Users,OU=CBCI,DC=officeSouth,DC=login" -Properties * | Select-Object name,department | export-csv -path c:\temp\userexportAll.csv
