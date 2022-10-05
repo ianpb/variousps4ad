cc63ea620430547e98b0520861d484585480b5c2
ianpb <ianpaul.budiman@gmail.com>
2022-10-05 10:37:44 +0800

    Create exprtgroupmembers.ps1

---
+++ exprtgroupmembers.ps1
@@ -0,0 +1,75 @@
+#Export Group Members from AD (not including disabled users)
+# Get year and month for CSV export file
+$DateTime = Get-Date -f "yyyyMMddhhmm"
+
+# Set CSV file name
+$CSVFile = "C:\temp\ADGroups_" + $DateTime + ".csv"
+
+# Set distinguishedName as searchbase, you can use one DN or multiple DNs
+# Or use the root domain like DC=exoip,DC=local
+$DNs = @(
+    "CN=PBF Request,OU=On-Premises Security Groups,OU=Security Groups,OU=Office 365,OU=CCI,DC=office,DC=login"
+)
+
+# Create empty array for CSV data
+$CSVOutput = @()
+
+# Create empty array for AD groups
+$ADGroups = @()
+
+# Loop through DNs
+foreach ($DN in $DNs) {
+
+    # Add every DN to AD groups
+    $ADGroups += Get-ADGroup -Filter * -properties * -SearchBase $DN
+}
+
+# Set progress bar variables
+$i = 0
+$tot = $ADGroups.count
+
+foreach ($ADGroup in $ADGroups) {
+
+    # Set up progress bar
+    $i++
+    $status = "{0:N0}" -f ($i / $tot * 100)
+    Write-Progress -Activity "Exporting AD Groups" -status "Processing Group $i of $tot : $status% Completed" -PercentComplete ($i / $tot * 100)
+
+    # Ensure Members variable is empty
+    $Members = ""
+
+    # Get group members which are also groups and add to string
+    $MembersArr = Get-ADGroup -filter { Name -eq $ADGroup.Name } | Get-ADGroupMember | select Name, objectClass, distinguishedName, UserPrincipalName
+    if ($MembersArr) {
+        foreach ($Member in $MembersArr) {
+            if ($Member.objectClass -eq "user") {
+                $MemDN = $Member.distinguishedName
+                $UserObj = Get-ADUser -filter { DistinguishedName -eq $MemDN }
+                if ($UserObj.Enabled -eq $False) {
+                    continue
+                }
+            }
+            $Members = $Members + "," + $Member.Name
+        }
+        # Check for members to avoid error for empty groups
+        if ($Members) {
+            $Members = $Members.Substring(1, ($Members.Length) - 1)
+        }
+    }
+
+    # Set up hash table and add values
+    $HashTab = $null
+    $HashTab = [ordered]@{
+        "Name"     = $ADGroup.Name
+        "Category" = $ADGroup.GroupCategory
+        "Description" = $ADGroup.Description
+        "Scope"    = $ADGroup.GroupScope
+        "Members"  = $Members
+    }
+
+    # Add hash table to CSV data array
+    $CSVOutput += New-Object PSObject -Property $HashTab
+}
+
+# Export report to CSV file
+$CSVOutput | Sort-Object Name | Export-Csv -Encoding UTF8 -Path $CSVFile -NoTypeInformation #-Delimiter ";"
